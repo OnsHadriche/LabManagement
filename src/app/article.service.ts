b9c34ef75d4dbaa369588026835a8982b16dbc4e
Khalilabid95 <khalilabid98@gmail.com>
2024-04-03 14:17:23 +0300

    Add new function GETALL()

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -8,8 +8,11 @@ import { GLOBAL } from 'src/assets/app-config';
   providedIn: 'root'
 })
 export class ArticleService {
-  tab1:any[]=GLOBAL.DB.articles;
+  tab1:Article[]=[];
   constructor(private httpClient:HttpClient) {}
+  GETALL():Observable<Article[]>{
+    return this.httpClient.get<Article[]>('http://localhost:3000/articles')
+  }
   ONSAVE(articleTosave :any):Observable<any>{
     //generateur de requete http en mod post
    // return this.httpClient.post('127.0.0.1:8080/api/Member',memberTosave)
