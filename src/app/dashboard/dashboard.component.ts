060b34d5bbbbe49e4f3101f5b255fd9cee1a6e80
Ons Hadrich <ounshadriche@gmail.com>
2024-04-24 18:30:57 +0300

    creating variables of numbers,articles and events

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventService } from 'src/Services/event-service.service';
+import { MemberService } from 'src/Services/member.service';
+import { ArticleService } from '../article.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  Nb_members !: number;
+  Nb_events !: number;
+  Nb_articles !: number;
+  Nb_outils !: number;
+  constructor(private MS:MemberService,private ES:EventService,private AS:ArticleService){
+
+  }
+  ngOnInit(): void {
+    this.getMembers();
+    this.getArticles();
+    this.getEents();
+  }
+  getArticles(){
+
+    this.AS.GETALL().subscribe((res)=>{
+      this.Nb_articles = res.length
+    })
+  }
+  getMembers(){
+    this.MS.GETALL().subscribe((res)=>{
+      this.Nb_members = res.length
+    })
+  }
+  getEents(){
+    this.MS.GETALL().subscribe((res)=>{
+      this.Nb_events = res.length
+    })
+  }
 
 }
