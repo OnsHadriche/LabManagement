8899dbf2a513736fce070293ff104fcd3d17ac78
Ons Hadrich <ounshadriche@gmail.com>
2024-04-24 17:41:59 +0300

    Add new attribute (table of articles, table of events)

--- src/Modeles/Member.ts
+++ src/Modeles/Member.ts
@@ -1,8 +1,13 @@
+import { Article } from "./Article";
+import { Evenement } from "./Evt";
+
 export interface Member{
     id:string,
     cin:string,
     name:string,
     type:string,
     cv:string,
-    createdDate:string
+    createdDate:string,
+    tab_pub : Article[],
+    tab_evt:Evenement[]
 }
